c72465faf34e812704cab18b3e0d025c24ef54cc
StarryNight00 <cisa.matias@gmail.com>
2019-04-12 11:28:35 +0100

    completada classe com um construtor e alguns métodos publicos + comments

--- BookManager/Book.cs
+++ BookManager/Book.cs
@@ -16,5 +16,26 @@ namespace BookManager
             this.title = title;
             this.author = author;
         }
+
+        //método com Lambda (=>) - simplifica retornos
+        //Retorna um STRING correspondente ao valor da instância title da Classe
+        public string GetTitle() => title;
+
+
+        //Este método é void porque está a especificar um valor da instância, não a retornar um valor
+        public void SetTitle(string title)
+        {
+            //Verificar se o title dado em argumento não é nulo
+            //e tem um comprimento maior que 0 caractéres 
+            if (title != null && title.Length > 0)
+                this.title = title;
+        }
+
+        public int GetTitleLength()
+        {
+            return title.Length;
+        }
+        //Em Lambda:
+        //public int GetTitleLength() => title.Length
     }
 }
