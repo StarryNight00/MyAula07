91bd8340971bdbbe3e66744cec55e4d4eb902388
StarryNight00 <cisa.matias@gmail.com>
2019-04-24 16:06:53 +0100

    Added Sets and Gets to the Color class

--- ColorBalls/Color.cs
+++ ColorBalls/Color.cs
@@ -6,10 +6,10 @@ namespace ColorBalls
 {
     internal class Color
     {
-        private int red;
-        private int green;
-        private int blue;
-        private int alpha;
+        private uint red;
+        private uint green;
+        private uint blue;
+        private uint alpha;
 
 
         private static int paletteCount;
@@ -20,7 +20,16 @@ namespace ColorBalls
             paletteCount = 0;
         }
 
-        public Color(int red, int green, int blue, int alpha)
+
+        /// <summary>
+        /// All parameter Color constructor. Receives 4 values.
+        /// All RGB color values are positive, usually in between 0 and 255.
+        /// </summary>
+        /// <param name="red"> uInt value for red.</param>
+        /// <param name="green"> uInt value for green.</param>
+        /// <param name="blue"> uInt value for blue.</param>
+        /// <param name="alpha">uInt value for alpha, or transparency.</param>
+        public Color(uint red, uint green, uint blue, uint alpha)
         {
             this.red = red;
             this.green = green;
@@ -30,15 +39,59 @@ namespace ColorBalls
             paletteCount++;
         }
 
-        public Color(int red, int green, int blue)
+        /// <summary>
+        /// Three parameter Color constructor. Receives 3 values. Alpha is set to opaque.
+        /// All RGB color values are positive, usually in between 0 and 255.
+        /// </summary>
+        /// <param name="red"> uInt value for red.</param>
+        /// <param name="green"> uInt value for green.</param>
+        /// <param name="blue"> uInt value for blue.</param>
+        public Color(uint red, uint green, uint blue)
         {
             this.red = red;
             this.green = green;
             this.blue = blue;
-            this.alpha = 255;
+            alpha = 255;
 
             paletteCount++;
         }
+
+
+        //'Sets' Group
+        public void SetRed(int red)
+        {
+            //Verificar se o valor red dado em argumento não é nulo
+            //e não tem valor negativo ou acima de 255
+            if (red >= 0 && red <= 255)
+                this.red = (uint)red;
+        }
+
+        public void SetGreen(int green)
+        {
+            if (green >= 0 && green <= 255)
+                this.green = (uint)green;
+        }
+
+        public void SetBlue(int blue)
+        {
+            if (blue >= 0 && blue <= 255)
+                this.blue = (uint)blue;
+        }
+
+        public void SetAlpha(int alpha)
+        {
+            if (alpha >= 0 && alpha <= 255)
+                this.alpha = (uint)alpha;
+        }
+
+        //'Gets' Group
+        public uint GetRed() => red;
+
+        public uint GetGreen() => green;
+
+        public uint GetBlue() => blue;
+
+        public uint GetAlpha() => alpha;
 
     }
 }
--- ColorBalls/Program.cs
+++ ColorBalls/Program.cs
@@ -6,7 +6,8 @@ namespace ColorBalls
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello");
         }
     }
 }
